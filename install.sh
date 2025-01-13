2b50d149227f97284ac8ce57b705e46a277bb32d
Robin Skoglund <robin.skoglund@bring.com>
2025-01-13 12:39:52 +0100

    Make install script idempotent

--- install.sh
+++ install.sh
@@ -20,25 +20,39 @@ abs_dirname() {
   cd "$cwd"
 }
 
+link_dotfile() {
+  local source="$1"
+  local target="$2"
+
+  if [[ -L "$target" ]]; then
+    echo "Already linked: $target"
+  else
+    echo "Linking $source -> $target"
+    ln -sv "$source" "$target"
+  fi
+}
+
 DOTFILES_DIR="$(abs_dirname "$0")"
 
-ln -s "$DOTFILES_DIR/.bash_aliases" "$HOME/.bash_aliases"
-ln -s "$DOTFILES_DIR/.bash_completion" "$HOME/.bash_completion"
-ln -s "$DOTFILES_DIR/.bash_completion.d" "$HOME/.bash_completion.d"
-ln -s "$DOTFILES_DIR/.bash_profile" "$HOME/.bash_profile"
-ln -s "$DOTFILES_DIR/.inputrc" "$HOME/.inputrc"
-ln -s "$DOTFILES_DIR/.profile" "$HOME/.profile"
-ln -s "$DOTFILES_DIR/.toprc" "$HOME/.toprc"
-ln -s "$DOTFILES_DIR/.vim" "$HOME/.vim"
-ln -s "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
-ln -s "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
+link_dotfile "$DOTFILES_DIR/.bash_aliases" "$HOME/.bash_aliases"
+link_dotfile "$DOTFILES_DIR/.bash_completion" "$HOME/.bash_completion"
+link_dotfile "$DOTFILES_DIR/.bash_completion.d" "$HOME/.bash_completion.d"
+link_dotfile "$DOTFILES_DIR/.bash_profile" "$HOME/.bash_profile"
+link_dotfile "$DOTFILES_DIR/.inputrc" "$HOME/.inputrc"
+link_dotfile "$DOTFILES_DIR/.profile" "$HOME/.profile"
+link_dotfile "$DOTFILES_DIR/.toprc" "$HOME/.toprc"
+link_dotfile "$DOTFILES_DIR/.vim" "$HOME/.vim"
+link_dotfile "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
+link_dotfile "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
 
 if [[ ! -d "$HOME/.config" ]]; then
-    mkdir "$HOME/.config"
+  echo "Create $HOME/.config directory"
+  mkdir "$HOME/.config"
 fi
-ln -s "$DOTFILES_DIR/starship.toml" "$HOME/.config/starship.toml"
+link_dotfile "$DOTFILES_DIR/starship.toml" "$HOME/.config/starship.toml"
 
 if [[ ! -d "$HOME/Library/Application\ Support/com.mitchellh.ghostty" ]]; then
+  echo "Create $HOME/Library/Application\ Support/com.mitchellh.ghostty"
   mkdir -p "$HOME/Library/Application\ Support/com.mitchellh.ghostty"
 fi
-ln -s "$DOTFILES_DIR/ghostty.config" "$HOME/Library/Application\ Support/com.mitchellh.ghostty/config"
+link_dotfile "$DOTFILES_DIR/ghostty.config" "$HOME/Library/Application\ Support/com.mitchellh.ghostty/config"
